b8336cd0f5bc8d092e35f40c08b80ab0f2cd3880
shilllabs <62120143+shilllabs@users.noreply.github.com>
2024-02-04 13:44:40 -0500

    Add files via upload

--- vm-ca-01-configure.ps1
+++ vm-ca-01-configure.ps1
@@ -3,13 +3,14 @@
 
 param 
 ( 
+    [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$Domain_DNSName,
     [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$win_username,
     [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [String]$win_userpass
 )
 start-transcript -Path "c:\logging2.txt"
 #$SMAP = ConvertTo-SecureString -AsPlainText $win_userpass -Force
 [SecureString]$secureString = $win_userpass | ConvertTo-SecureString -AsPlainText -Force
-[PSCredential]$credentialObject = New-Object System.Management.Automation.PSCredential -ArgumentList "$win_username@shilllabs.cloud", $secureString
+[PSCredential]$credentialObject = New-Object System.Management.Automation.PSCredential -ArgumentList "$win_username@$Domain_DNSName", $secureString
 out-file c:\temp\creds2.txt -InputObject $credentialObject
 out-file c:\temp\inputvariables2.txt -InputObject $win_username, $win_userpass
 
